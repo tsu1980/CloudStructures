ca68029ef7b15eb92c76ce7a8e6f55b3ebf96216
tsu1980 <tsu1980@gmail.com>
2017-12-08 11:56:13 +0900

    Add Json.NET RedisValueConverter as default. Move Jil RedisValueConverter to separate project.

---
+++ CloudStructures.Jil/GZipJilRedisValueConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudStructures.Jil
+{
+    public class GZipJilRedisValueConverter : ObjectRedisValueConverterBase
+    {
+        readonly System.IO.Compression.CompressionLevel compressionLevel;
+
+        public GZipJilRedisValueConverter()
+            : this(System.IO.Compression.CompressionLevel.Fastest)
+        {
+
+        }
+
+        public GZipJilRedisValueConverter(System.IO.Compression.CompressionLevel compressionLevel)
+        {
+            this.compressionLevel = compressionLevel;
+        }
+
+        protected override object DeserializeCore(Type type, byte[] value)
+        {
+            using (var ms = new MemoryStream(value))
+            using (var gzip = new System.IO.Compression.GZipStream(ms, System.IO.Compression.CompressionMode.Decompress))
+            using (var sr = new StreamReader(gzip, Encoding.UTF8))
+            {
+                var result = global::Jil.JSON.Deserialize(sr, type);
+                return result;
+            }
+        }
+
+        protected override byte[] SerializeCore(object value, out long resultSize)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var gzip = new System.IO.Compression.GZipStream(ms, compressionLevel))
+                using (var sw = new StreamWriter(gzip))
+                {
+                    global::Jil.JSON.Serialize(value, sw);
+                }
+                var result = ms.ToArray();
+                resultSize = result.Length;
+                return result;
+            }
+        }
+    }
+}
---
+++ CloudStructures.Jil/JilRedisValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudStructures.Jil
+{
+    public class JilRedisValueConverter : ObjectRedisValueConverterBase
+    {
+        protected override object DeserializeCore(Type type, byte[] value)
+        {
+            using (var ms = new MemoryStream(value))
+            using (var sr = new StreamReader(ms, Encoding.UTF8))
+            {
+                var result = global::Jil.JSON.Deserialize(sr, type);
+                return result;
+            }
+        }
+
+        protected override byte[] SerializeCore(object value, out long resultSize)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var sw = new StreamWriter(ms, Encoding.UTF8))
+                {
+                    global::Jil.JSON.Serialize(value, sw);
+                }
+                var result = ms.ToArray();
+                resultSize = result.Length;
+                return result;
+            }
+        }
+    }
+}
